61d893708c1ce82e19b73cd045f15136d82404d3
github-usernamebelifegor <hydrox27@gmail.com>
2025-04-30 17:49:23 +0300

    chore: configure husky with eslint and prettier

--- .husky/_/husky.sh
+++ .husky/_/husky.sh
@@ -1,3 +1,9 @@
-#!/bin/sh
-# Husky
-# v9+ bootstrap
+echo "husky - DEPRECATED
+
+Please remove the following two lines from $0:
+
+#!/usr/bin/env sh
+. \"\$(dirname -- \"\$0\")/_/husky.sh\"
+
+They WILL FAIL in v10.0.0
+"

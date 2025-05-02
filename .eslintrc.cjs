a6b184e12343dd156bfb9322de20ac8f435a77ec
github-usernamebelifegor <hydrox27@gmail.com>
2025-05-02 16:29:15 +0300

    chore: enforce no-explicit-any as error in ESLint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -34,7 +34,7 @@ module.exports = {
   rules: {
     'react/react-in-jsx-scope': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/no-explicit-any': 'warn',
+    '@typescript-eslint/no-explicit-any': 'error',
     'react/prop-types': 'off',
     'import/order': ['warn', { groups: ['builtin', 'external', 'internal'] }],
   },

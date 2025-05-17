7834380e1353ebcdf41ca8cbd37634765f938f13
github-usernamebelifegor <hydrox27@gmail.com>
2025-05-17 14:56:20 +0300

    refactor: add new styles fot button to fix width on small devices

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -13,7 +13,7 @@ type Product = {
 export function ProductCard({ name, price, image, liked }: Product) {
   const [isLiked, setIsLiked] = useState(liked ?? false);
   return (
-    <div className="relative  bg-[#F6F6F6] rounded-xl px-4 flex flex-col items-center text-center shadow hover:shadow-lg transition">
+    <div className="relative  bg-[#F6F6F6] rounded-xl px-4 flex flex-col items-center text-center shadow hover:shadow-lg transition h-full">
       {/* иконка лайка */}
       <div className="w-full mb-4 mt-4 flex justify-end">
         <button onClick={() => setIsLiked(!isLiked)}>
@@ -27,14 +27,16 @@ export function ProductCard({ name, price, image, liked }: Product) {
       {/* изображение товара */}
       <img src={image} alt={name} className="w-full h-40 object-contain mb-4" />
       {/* название товара */}
-      <h3 className="text-lg font-medium text-gray-800 mb-4 leading-snug  whitespace-pre-line">
-        {name}
-      </h3>
-      {/* цена товара */}
-      <p className="text-lg font-bold mb-3">${price.toFixed(2)}</p>
+      <div className="flex flex-col flex-grow justify-between">
+        <h3 className="text-lg font-medium text-gray-800 mb-4 leading-snug  whitespace-pre-line">
+          {name}
+        </h3>
+        {/* цена товара */}
+        <p className="text-lg font-bold mb-3">${price.toFixed(2)}</p>
+      </div>
       {/* кнопка */}
-      <button className="bg-black  text-white mb-6 px-16 py-3 rounded-lg hover:bg-[#9a2ee8] hover:text-white transition">
-        Buy now
+      <button className="mt-auto bg-black  text-white w-full max-w-[200px] mb-6 px-16 py-3 rounded-lg hover:bg-[#9a2ee8] hover:text-white transition">
+        <span className="whitespace-nowrap">Buy now</span>
       </button>
     </div>
   );

ba42a97541a19a040aab783063972f8ca35af301
AleksandrKlesh <alex73kl@gmail.com>
2025-05-18 19:47:24 +0700

    fix: add router to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,6 @@
+import { RouterProvider } from 'react-router-dom';
+import router from './routes/router';
+
 export default function App() {
-  return (
-    <div className="min-h-screen bg-dark-100 flex items-center justify-center">
-      <h1 className="text-4xl font-bold text-purple-600">
-        TailwindCSS v4 работает! Ура!
-      </h1>
-    </div>
-  );
+  return <RouterProvider router={router} />;
 }

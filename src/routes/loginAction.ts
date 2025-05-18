3186cdccd3defff86b247852986ac3c694cb8513
AleksandrKlesh <alex73kl@gmail.com>
2025-05-18 20:15:27 +0700

    feat: add login action

---
+++ src/routes/loginAction.ts
@@ -0,0 +1,22 @@
+import { ActionFunctionArgs, redirect } from 'react-router-dom';
+import { loginCustomer } from '../services/sdk/loginCustomer';
+import { authStore } from '../store/store';
+
+export async function loginAction({ request }: ActionFunctionArgs) {
+  const formData = await request.formData();
+  const email = formData.get('email');
+  const password = formData.get('password');
+
+  try {
+    if (typeof email === 'string' && typeof password === 'string') {
+      const result = await loginCustomer({ email, password });
+      authStore.getState().login(result.customer);
+
+      return redirect('/main');
+    }
+  } catch (error) {
+    console.error('Login failed', error);
+
+    return error;
+  }
+}

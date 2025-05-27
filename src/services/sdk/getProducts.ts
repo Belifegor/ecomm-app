514e45e618a78e54e11e6fc6b43e7918c76c460b
AleksandrKlesh <alex73kl@gmail.com>
2025-05-28 00:36:34 +0700

    fix: add limit to 10

--- src/services/sdk/getProducts.ts
+++ src/services/sdk/getProducts.ts
@@ -1,7 +1,11 @@
-import { apiRoot } from './apiRoot';
+import { apiRoot } from '../CommerceTools/BuildClient';
 
 export async function getProducts() {
-  const response = await apiRoot.productProjections().get().execute();
+  const response = await apiRoot
+    .productProjections()
+    .get({ queryArgs: { limit: 10 } })
+    .execute();
 
+  console.log('Get product projections from GetProducts', response);
   return response.body.results;
 }

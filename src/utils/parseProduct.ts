d3fd2cdce3351e448c8b6b14f02ea63902400244
AleksandrKlesh <alex73kl@gmail.com>
2025-05-30 18:05:08 +0700

    fix: add format price

--- src/utils/parseProduct.ts
+++ src/utils/parseProduct.ts
@@ -1,6 +1,14 @@
 import { ProductProjection } from '@commercetools/platform-sdk';
 import { Product } from '../components/CatalogCard';
 
+function formatPrice(centAmount?: number): string {
+  if (centAmount) {
+    return `${(centAmount / 100).toFixed(2)}`;
+  } else {
+    return `undefined`;
+  }
+}
+
 export function parseProduct(product: ProductProjection): Product {
   const master = product.masterVariant;
   const priceInfo = master?.prices?.[0];
@@ -13,7 +21,7 @@ export function parseProduct(product: ProductProjection): Product {
     name: product.name['en-US'],
     description: product.description?.['en-US'],
     imageUrl,
-    price: `${currentPrice?.centAmount}`,
-    originalPrice: `${originalPrice?.centAmount}`,
+    price: formatPrice(currentPrice?.centAmount),
+    originalPrice: formatPrice(originalPrice?.centAmount),
   };
 }

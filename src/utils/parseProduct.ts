2c8e6558e1d1d7068ca25274ad55b76ffe1b6305
kateNEO <glebova.caterina2012@yandex.ru>
2025-06-16 23:24:02 +0200

    feat: adding about us page refactoring: delete commented file and rename actual

--- src/utils/parseProduct.ts
+++ src/utils/parseProduct.ts
@@ -1,5 +1,5 @@
 import { ProductProjection } from '@commercetools/platform-sdk';
-import { Product } from '../components/CatalogCard_merged';
+import { Product } from '../components/CatalogCard.tsx';
 
 function formatPrice(centAmount?: number): string {
   if (centAmount) {
